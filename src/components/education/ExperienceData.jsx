551720ced6c2d6bc59b94e9890586ac996ca462d
Manoj Kumar. M <97655367+manoj-kumar-m@users.noreply.github.com>
2023-09-06 07:22:58 +0530

    Update ExperienceData.jsx

--- src/components/education/ExperienceData.jsx
+++ src/components/education/ExperienceData.jsx
@@ -3,25 +3,25 @@ const ExperienceData = [
     {
       id: 1,
       category: "experience",
-      year: "Internship (2022 - present) ",
-      title: "Samsung Internship ",
-      desc: "currently interning at Samsung R & D Institute, India under PRISM program",
+      year: "Internship (Jun-2022 to present) ",
+      title: "Azul Systems",
+      desc: "Junior Software Engineering Intern at Azul Systems",
       
     },
     {
       id: 2,
       category: "experience",
-      year: "Internship ((2022 - present) ",
-      title: "Samsung Intern PRISM  Program",
-      desc: "currently interning at Samsung R & D Institute, India under PRISM program",
+      year: "Internship (Feb-2023 to May-2023) ",
+      title: "Cogoport",
+      desc: "Software Development Engineer - I at Cogoport. Worked as a React Developer",
       
     },
     {
       id: 3,
       category: "experience",
-      year: "Internship (2022 - present) ",
+      year: "Internship (May-2022 to Nov-2022) ",
       title: "Samsung Intern  PRISM  Program",
-      desc: "currently interning at Samsung R & D Institute, India under PRISM program",
+      desc: "Full Stack Development Research Intern under PRISM program",
       
     },
   
